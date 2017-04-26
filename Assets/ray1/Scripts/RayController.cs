9feaa8082ecd124f01cecc11fd45ca65405479bf
Takuma Saito <takuma_saito@TakumanoMacBook-Pro.local>
2017-04-26 12:54:26 +0900

    init

---
+++ Assets/ray1/Scripts/RayController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+    void Update () {
+        if(Input.GetMouseButtonDown(0)){
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit)) {
+                GameObject selectedObj = hit.collider.gameObject;
+                print("name: " + selectedObj.name + selectedObj.transform.position);
+            }
+        }
+    }
+}
---
+++ Assets/ray2/Scripts/RayController2.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayController2 : MonoBehaviour {
+
+    public GameObject player;
+    Vector3 newPlayerPosition;
+    private float time;
+
+	
+	void Update () {
+        if(Input.GetMouseButtonDown(0)) {
+            time = 0;
+            Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+            RaycastHit hit = new RaycastHit ();
+
+            if(Physics.Raycast(ray, out hit)) {
+                newPlayerPosition = hit.point;
+            }
+        }
+        time += Time.deltaTime * 0.1f;
+        player.transform.position = Vector3.Lerp (player.transform.position, newPlayerPosition, time);
+    }
+}
